da44616e0691ba9e66ca38054527b22e157d07d7
dmostroff <ostroffent@gmail.com>
2021-06-25 16:17:19 +0300

    vue form

--- templates/vue_form_component.tmplt.vue
+++ templates/vue_form_component.tmplt.vue
@@ -1,55 +1,87 @@
 <template>
 <v-form>
   <v-card>
-    <v-card-header :md-elevation="2">
+    <v-card-title class="primary white--text">
       {{table.title}}
-    </v-card-header>
+    </v-card-title>
     <v-card-text>
       <v-container>
         {% for col in table.columns %}<v-row>
-          <v-col cols="12" md="4">
             {% if loop.index == 1 %}
-            <span class="caption">{{table.labels[loop.index-1]}}</span>
-            <span class="">{{'{{'}}{{ table.object }}.{{col}} {{'}}'}}</span>
-            {% else %}<v-text-field>
+              <v-col cols="2" class="caption">
+                Id: {{'{{'}}{{table.object}}.id {{'}}'}}
+                </v-col>
+              <v-spacer></v-spacer>
+              <v-col cols="3" class="caption">
+                Recorded on: {{'{{'}} formatDateTime({{table.object}}.recorded_on) {{'}}'}}
+              </v-col>
+            {% else %}
+            <v-col cols="2">
+            <v-text-field
               v-model="{{table.object}}.{{ col }}"
-              label="{{table.labels[loop.index]}}"
+              label="{{table.labels[loop.index-1]}}"
+              :readonly="isReadOnly"
+              >
             </v-text-field>
-            {% endif %}
-          </v-col>
+          </v-col>{% endif %}
         </v-row>{% endfor %}
       </v-container>
     </v-card-text>
-    <v-card-actions md-alignment="right">
-      <v-btn @click="editForm">Edit</v-btn>
-      <v-btn @click="cancelForm">Cancel</v-btn>
+    <v-card-actions>
+      <EditSaveCancelBtn
+        :isReadOnly="isReadOnly"
+        @editForm="editForm"
+        @saveForm="saveForm"
+        @cancelForm="cancelForm"
+        @closeForm="closeForm"
+      ></EditSaveCancelBtn>
     </v-card-actions>
   </v-card>
   </v-form>
 </template>
 
 <script>
+import commonService from "@/services/commonService";
+// import admService from '@/services/admService'
+import {{table.object}}Service from "@/services/{{table.object}}Service";
+import EditSaveCancelBtn from "@/components/common/EditSaveCancelBtn";
 
 export default {
   value: "{{table.class}}",
   components: {
+    EditSaveCancelBtn
   },
   props: {
-    {{table.object}}: Object
+    {{table.object}}: Object,
   },
   data() {
     return {
+      isReadOnly: true,
     };
   },
   computed: {},
-  mounted() {},
+  mounted() {
+    this.prev{{table.class}} = commonService.clone( this.{{table.object}})
+  },
   methods: {
+    formatDateTime(datetime) {
+      return commonService.formatDateTime(datetime);
+    },
     editForm() {
-      this.$emit( "edit{{table.class}}Form" )
+      this.isReadOnly = false
+    },
+    async saveForm() {
+      let {{table.object}} = await {{table.object}}Service.post{{table.class}}(this.{{table.object}})
+      this.isReadOnly = true
+      this.$emit("saveForm", {{table.object}});
     },
     cancelForm() {
-      this.$emit( "cancel{{table.class}}Form" )
-    }
+      this.isReadOnly = true
+      this.{{table.object}} = commonService.clone( this.prev{{table.class}})
+    },
+    closeForm() {
+      this.$emit("cancelForm")
+    },
   },
   created() {},
 };
